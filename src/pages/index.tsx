5c7e4d2be411492109391a8c77ab06070074f38e
Minh <minh.nguyen@ellie.ai>
2025-03-11 21:19:17 +0200

    add temp index file for deployment testing

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+import { Box } from "@mui/material";
+export default function HomePage() {
+  return (
+    <Box>
+      <Typography variant="h1">Home</Typography>
+    </Box>
+  );
+}
